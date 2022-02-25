af502fb36360b107db8dd581167f931936bd430b
Elahe Taheri <91569942+Telahe@users.noreply.github.com>
2022-02-26 03:12:35 +0330

    Add files via upload

---
+++ theory-lab-BMI.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+weight = float(input('enter the weight(kg): '))
+height = float(input('enter the height(m): '))
+
+bmi = weight/height**2
+
+if bmi < 18.5:
+    print('underweight')
+elif bmi < 24.9 and bmi >= 18.5:
+    print('normal')
+elif bmi < 29.9 and bmi >= 25:
+    print('overweight')
+elif bmi <35 and bmi >= 30:
+    print('obese')
+elif bmi>= 35:
+    print('extremely obese')
+
