7ca96846ed6f9e13cb0a67849f138802709f6bb7
Elahe Taheri <91569942+Telahe@users.noreply.github.com>
2022-02-26 02:58:25 +0330

    Add files via upload

---
+++ theory-lab-is-triangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+num1 = int(input('enter first number: '))
+num2 = int(input('enter second number: '))
+num3 = int(input('enter third number: '))
+
+if num1< num2+num3 and num2<num1+num3 and num3<num1+num2:
+    print('this is a triangle')
+else:
+    print('it is not a triangle')
+
